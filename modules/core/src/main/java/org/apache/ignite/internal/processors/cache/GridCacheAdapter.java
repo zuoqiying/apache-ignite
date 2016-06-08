63c072c61aea5bdd05a6c7f47baad868128ebdbf
sboikov <sboikov@gridgain.com>
2016-06-08 12:12:15 +0300

    minor

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
@@ -163,7 +163,8 @@ public abstract class GridCacheAdapter<K, V> implements IgniteInternalCache<K, V
     public static final int CLEAR_ALL_SPLIT_THRESHOLD = 10000;
 
     /** Size of keys batch to removeAll. */
-    public static final int REMOVE_ALL_KEYS_BATCH = 10000;
+    // TODO GG-11231 (workaround for GG-11231).
+    private static final int REMOVE_ALL_KEYS_BATCH = 10000;
 
     /** Maximum number of retries when topology changes. */
     public static final int MAX_RETRIES = IgniteSystemProperties.getInteger(IGNITE_CACHE_RETRIES_COUNT, 100);
@@ -2961,20 +2962,21 @@ public abstract class GridCacheAdapter<K, V> implements IgniteInternalCache<K, V
     @Override public void removeAll() throws IgniteCheckedException {
         assert ctx.isLocal();
 
-        // Workaround for GG-11231.
-        if(isEmpty())
+        // TODO GG-11231 (workaround for GG-11231).
+        if (isEmpty())
             return;
 
         List<K> keys = new ArrayList<>(Math.min(REMOVE_ALL_KEYS_BATCH, size()));
+
         while (!isEmpty()) {
             for (Iterator<CacheDataRow> it = ctx.offheap().iterator(true, true, null);
-                it.hasNext() && keys.size() < REMOVE_ALL_KEYS_BATCH; )
+                it.hasNext() && keys.size() < REMOVE_ALL_KEYS_BATCH;)
                 keys.add((K)it.next().key());
 
             removeAll(keys);
 
             keys.clear();
-        } // End of workaround for GG-11231.
+        }
     }
 
     /** {@inheritDoc} */
