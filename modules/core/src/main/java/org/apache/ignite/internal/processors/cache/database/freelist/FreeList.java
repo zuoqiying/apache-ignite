9cde88e92f59ed0b4b73805de11e6ca2e279fcdd
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2016-08-23 18:35:29 +0300

    Move code around for easier review.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/database/freelist/FreeList.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/database/freelist/FreeList.java
@@ -70,6 +70,164 @@ public class FreeList {
     private final ConcurrentHashMap8<Integer, GridFutureAdapter<FreeTree>> trees = new ConcurrentHashMap8<>();
 
     /**
+     *
+     */
+    private class WriteRowPageHandler extends PageHandler<CacheDataRow, Integer> {
+        /** */
+        private final GridCacheContext cctx;
+
+        /**
+         *
+         */
+        private WriteRowPageHandler(GridCacheContext cctx) {
+            this.cctx = cctx;
+        }
+
+        /** {@inheritDoc} */
+        @Override public Integer run(long pageId, Page page, ByteBuffer buf, CacheDataRow row, int written)
+            throws IgniteCheckedException {
+            DataPageIO io = DataPageIO.VERSIONS.forPage(buf);
+
+            CacheObjectContext coctx = cctx.cacheObjectContext();
+
+            int rowSize = getRowSize(coctx, row);
+            int freeSpace = io.getFreeSpace(buf);
+
+            assert freeSpace > 0 : freeSpace;
+
+            // If the full row does not fit into this page write only a fragment.
+            written = freeSpace >= rowSize ? addRow(coctx, page, buf, io, row, rowSize) :
+                addRowFragment(coctx, page, buf, io, row, written, rowSize);
+
+            // Reread free space after update.
+            freeSpace = io.getFreeSpace(buf);
+
+            // Put our free item back to tree.
+            tree(cctx, row.partition()).put(new FreeItem(freeSpace, pageId));
+
+            // Avoid boxing with garbage generation for usual case.
+            return written == rowSize ? COMPLETE : written;
+        }
+
+        /**
+         * @param coctx Cache object context.
+         * @param page Page.
+         * @param buf Buffer.
+         * @param io IO.
+         * @param row Row.
+         * @param rowSize Row size.
+         * @return Written size which is always equal to row size here.
+         * @throws IgniteCheckedException If failed.
+         */
+        private int addRow(
+            CacheObjectContext coctx,
+            Page page,
+            ByteBuffer buf,
+            DataPageIO io,
+            CacheDataRow row,
+            int rowSize
+        ) throws IgniteCheckedException {
+            io.addRow(coctx, buf, row, rowSize);
+
+            // TODO This record must contain only a reference to a logical WAL record with the actual data.
+            if (isWalDeltaRecordNeeded(wal, page))
+                wal.log(new DataPageInsertRecord(cctx.cacheId(), page.id(),
+                    row.key(), row.value(), row.version(), rowSize));
+
+            return rowSize;
+        }
+
+        /**
+         * @param coctx Cache object context.
+         * @param page Page.
+         * @param buf Buffer.
+         * @param io IO.
+         * @param row Row.
+         * @param written Written size.
+         * @param rowSize Row size.
+         * @return Updated written size.
+         * @throws IgniteCheckedException If failed.
+         */
+        private int addRowFragment(
+            CacheObjectContext coctx,
+            Page page,
+            ByteBuffer buf,
+            DataPageIO io,
+            CacheDataRow row,
+            int written,
+            int rowSize
+        ) throws IgniteCheckedException {
+            // Read last link before the fragment write, because it will be updated there.
+            long lastLink = row.link();
+
+            int payloadSize = io.addRowFragment(coctx, buf, row, written, rowSize);
+
+            assert payloadSize > 0 : payloadSize;
+
+            if (isWalDeltaRecordNeeded(wal, page)) {
+                // TODO This record must contain only a reference to a logical WAL record with the actual data.
+                byte[] payload = new byte[payloadSize];
+
+                io.setPositionAndLimitOnPayload(buf, PageIdUtils.itemId(row.link()));
+                buf.get(payload);
+                buf.position(0);
+
+                wal.log(new DataPageInsertFragmentRecord(cctx.cacheId(), page.id(), payload, lastLink));
+            }
+
+            return written + payloadSize;
+        }
+    }
+
+    /**
+     *
+     */
+    private class RemoveRowPageHandler extends PageHandler<FreeTree, Long> {
+        /** */
+        private final GridCacheContext cctx;
+
+        /**
+         *
+         */
+        private RemoveRowPageHandler(GridCacheContext cctx) {
+            this.cctx = cctx;
+        }
+
+        /** {@inheritDoc} */
+        @Override public Long run(long pageId, Page page, ByteBuffer buf, FreeTree tree, int itemId) throws IgniteCheckedException {
+            assert tree != null;
+
+            DataPageIO io = DataPageIO.VERSIONS.forPage(buf);
+
+            int oldFreeSpace = io.getFreeSpace(buf);
+
+            assert oldFreeSpace >= 0: oldFreeSpace;
+
+            long nextLink = io.removeRow(buf, (byte)itemId);
+
+            if (isWalDeltaRecordNeeded(wal, page))
+                wal.log(new DataPageRemoveRecord(cctx.cacheId(), page.id(), itemId));
+
+            int newFreeSpace = io.getFreeSpace(buf);
+
+            // Move page to the new position with respect to the new free space.
+            FreeItem item = tree.remove(new FreeItem(oldFreeSpace, pageId));
+
+            // If item is null, then it was removed concurrently by insertRow, because
+            // in removeRow we own the write lock on this page. Thus we can be sure that
+            // insertRow will update position correctly after us.
+            if (item != null) {
+                FreeItem old = tree.put(new FreeItem(newFreeSpace, pageId));
+
+                assert old == null;
+            }
+
+            // For common case boxed 0L will be cached inside of Long, so no garbage will be produced.
+            return nextLink;
+        }
+    }
+
+    /**
      * @param wal Write-ahead log manager.
      * @param pageMem Page memory.
      * @param reuseList Reuse list.
@@ -239,164 +397,5 @@ public class FreeList {
         long pageId = pageMem.allocatePage(cacheId, part, PageIdAllocator.FLAG_DATA);
 
         return pageMem.page(cacheId, pageId);
-    }
-
-    /**
-     *
-     */
-    private class WriteRowPageHandler extends PageHandler<CacheDataRow, Integer> {
-        /** */
-        private final GridCacheContext cctx;
-
-        /**
-         *
-         */
-        public WriteRowPageHandler(GridCacheContext cctx) {
-            this.cctx = cctx;
-        }
-
-        /** {@inheritDoc} */
-        @Override public Integer run(long pageId, Page page, ByteBuffer buf, CacheDataRow row, int written)
-            throws IgniteCheckedException {
-            DataPageIO io = DataPageIO.VERSIONS.forPage(buf);
-
-            CacheObjectContext coctx = cctx.cacheObjectContext();
-
-            int rowSize = getRowSize(coctx, row);
-            int freeSpace = io.getFreeSpace(buf);
-
-            assert freeSpace > 0 : freeSpace;
-
-            // If the full row does not fit into this page write only a fragment.
-            written = freeSpace >= rowSize ? addRow(coctx, page, buf, io, row, rowSize) :
-                addRowFragment(coctx, page, buf, io, row, written, rowSize);
-
-            // Reread free space after update.
-            freeSpace = io.getFreeSpace(buf);
-
-            // Put our free item back to tree.
-            tree(cctx, row.partition()).put(new FreeItem(freeSpace, pageId));
-
-            // Avoid boxing with garbage generation for usual case.
-            return written == rowSize ? COMPLETE : written;
-        }
-
-        /**
-         * @param coctx Cache object context.
-         * @param page Page.
-         * @param buf Buffer.
-         * @param io IO.
-         * @param row Row.
-         * @param rowSize Row size.
-         * @return Written size which is always equal to row size here.
-         * @throws IgniteCheckedException If failed.
-         */
-        private int addRow(
-            CacheObjectContext coctx,
-            Page page,
-            ByteBuffer buf,
-            DataPageIO io,
-            CacheDataRow row,
-            int rowSize
-        ) throws IgniteCheckedException {
-            io.addRow(coctx, buf, row, rowSize);
-
-            // TODO This record must contain only a reference to a logical WAL record with the actual data.
-            if (isWalDeltaRecordNeeded(wal, page))
-                wal.log(new DataPageInsertRecord(cctx.cacheId(), page.id(),
-                    row.key(), row.value(), row.version(), rowSize));
-
-            return rowSize;
-        }
-
-        /**
-         * @param coctx Cache object context.
-         * @param page Page.
-         * @param buf Buffer.
-         * @param io IO.
-         * @param row Row.
-         * @param written Written size.
-         * @param rowSize Row size.
-         * @return Updated written size.
-         * @throws IgniteCheckedException If failed.
-         */
-        private int addRowFragment(
-            CacheObjectContext coctx,
-            Page page,
-            ByteBuffer buf,
-            DataPageIO io,
-            CacheDataRow row,
-            int written,
-            int rowSize
-        ) throws IgniteCheckedException {
-            // Read last link before the fragment write, because it will be updated there.
-            long lastLink = row.link();
-
-            int payloadSize = io.addRowFragment(coctx, buf, row, written, rowSize);
-
-            assert payloadSize > 0 : payloadSize;
-
-            if (isWalDeltaRecordNeeded(wal, page)) {
-                // TODO This record must contain only a reference to a logical WAL record with the actual data.
-                byte[] payload = new byte[payloadSize];
-
-                io.setPositionAndLimitOnPayload(buf, PageIdUtils.itemId(row.link()));
-                buf.get(payload);
-                buf.position(0);
-
-                wal.log(new DataPageInsertFragmentRecord(cctx.cacheId(), page.id(), payload, lastLink));
-            }
-
-            return written + payloadSize;
-        }
-    }
-
-    /**
-     *
-     */
-    private class RemoveRowPageHandler extends PageHandler<FreeTree, Long> {
-        /** */
-        private final GridCacheContext cctx;
-
-        /**
-         *
-         */
-        public RemoveRowPageHandler(GridCacheContext cctx) {
-            this.cctx = cctx;
-        }
-
-        /** {@inheritDoc} */
-        @Override public Long run(long pageId, Page page, ByteBuffer buf, FreeTree tree,
-            int itemId) throws IgniteCheckedException {
-            assert tree != null;
-
-            DataPageIO io = DataPageIO.VERSIONS.forPage(buf);
-
-            int oldFreeSpace = io.getFreeSpace(buf);
-
-            assert oldFreeSpace >= 0: oldFreeSpace;
-
-            long nextLink = io.removeRow(buf, (byte)itemId);
-
-            if (isWalDeltaRecordNeeded(wal, page))
-                wal.log(new DataPageRemoveRecord(cctx.cacheId(), page.id(), itemId));
-
-            int newFreeSpace = io.getFreeSpace(buf);
-
-            // Move page to the new position with respect to the new free space.
-            FreeItem item = tree.remove(new FreeItem(oldFreeSpace, pageId));
-
-            // If item is null, then it was removed concurrently by insertRow, because
-            // in removeRow we own the write lock on this page. Thus we can be sure that
-            // insertRow will update position correctly after us.
-            if (item != null) {
-                FreeItem old = tree.put(new FreeItem(newFreeSpace, pageId));
-
-                assert old == null;
-            }
-
-            // For common case boxed 0L will be cached inside of Long, so no garbage will be produced.
-            return nextLink;
-        }
     }
 }
