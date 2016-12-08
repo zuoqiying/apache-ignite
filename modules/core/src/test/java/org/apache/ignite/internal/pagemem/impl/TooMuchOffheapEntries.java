cbd84bb354c5edc617203dc77c112f7323e5f7ea
Alexandr Kuramshin <akuramshin@gridgain.com>
2016-12-09 00:00:05 +0700

    parallel loading

--- modules/core/src/test/java/org/apache/ignite/internal/pagemem/impl/TooMuchOffheapEntries.java
+++ modules/core/src/test/java/org/apache/ignite/internal/pagemem/impl/TooMuchOffheapEntries.java
@@ -18,6 +18,7 @@ package org.apache.ignite.internal.pagemem.impl;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteDataStreamer;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.cache.CacheMemoryMode;
 import org.apache.ignite.configuration.CacheConfiguration;
@@ -26,6 +27,9 @@ import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 
 import java.util.Arrays;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Executors;
 
 /**
  * TooMuchOffheapEntries
@@ -39,6 +43,9 @@ public class TooMuchOffheapEntries {
 
     /** Cache name */
     private static final String CACHE_NAME = "TooMuchOffheapEntriesCache";
+
+    /** Entries pack size */
+    private static final long PACK_SIZE = 1_000_000L;
 
     /** Create config */
     private static IgniteConfiguration config(String gridName) {
@@ -60,21 +67,58 @@ public class TooMuchOffheapEntries {
     }
 
     /** Main */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         IgniteConfiguration cfg = config(GRID_NAME);
         try (Ignite ignite = Ignition.start(cfg)) {
             IgniteCache<CacheKey, CacheKey> cache = ignite.cache(CACHE_NAME);
             long cnt = 0;
-            CacheKey key = new CacheKey();
+            CompletionService cs = new ExecutorCompletionService(Executors.newCachedThreadPool());
             System.out.println("Loading the cache");
+            int procs = Runtime.getRuntime().availableProcessors();
+            long portion = PACK_SIZE / procs;
+            long rem = PACK_SIZE % procs;
             while (true) {
-                for (int i = 0; i < 1_000_000L; ++i) {
-                    ++cnt;
-                    key.val = cnt;
-                    cache.put(key, key);
+                for (int i = 1; i <= procs; ++i) {
+                    long end = cnt + portion;
+                    if (i == procs)
+                        end += rem;
+                    cs.submit(new LoadCacheTask(ignite, cnt, end), null);
+                    cnt = end;
                 }
-                System.out.println(cnt);
+                for (int i = 0; i < procs; ++i)
+                    cs.take().get();
+                System.out.println(((double)cache.sizeLong()) / PACK_SIZE + " M");
             }
+        }
+    }
+
+    /** Loads cache with key interval */
+    private static class LoadCacheTask implements Runnable {
+
+        /** Ignite instance */
+        private final Ignite ignite;
+
+        /** Key interval start (including) */
+        private final long start;
+        /** Key interval end (excluding) */
+        private final long end;
+
+        /** Constructor */
+        public LoadCacheTask(Ignite ignite, long start, long end) {
+            this.ignite = ignite;
+            this.start = start;
+            this.end = end;
+        }
+
+        /** Loads the cache */
+        @Override public void run() {
+            IgniteDataStreamer<CacheKey, CacheKey> ds = ignite.dataStreamer(CACHE_NAME);
+            CacheKey key = new CacheKey();
+            for (long i = start; i < end; ++i) {
+                key.val = i;
+                ds.addData(key, key);
+            }
+            ds.flush();
         }
     }
 
@@ -83,5 +127,13 @@ public class TooMuchOffheapEntries {
 
         /** Key value */
         public long val;
+
+        @Override public boolean equals(Object obj) {
+            return obj == this || (obj instanceof CacheKey && ((CacheKey)obj).val == val);
+        }
+
+        @Override public int hashCode() {
+            return Long.hashCode(val);
+        }
     }
 }
