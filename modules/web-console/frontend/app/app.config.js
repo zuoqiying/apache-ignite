c533f174088ba844923d0070e2dbea83b8da53cb
Andrey Novikov <anovikov@gridgain.com>
2016-12-15 17:20:40 +0700

    IGNITE-4317 Migrate to angular 1.6

--- modules/web-console/frontend/app/app.config.js
+++ modules/web-console/frontend/app/app.config.js
@@ -29,6 +29,10 @@ import alertTemplateUrl from '../views/templates/alert.jade';
 
 const igniteConsoleCfg = angular.module('ignite-console.config', ['ngAnimate', 'mgcrea.ngStrap']);
 
+igniteConsoleCfg.config(['$compileProvider', ($compileProvider) => {
+    $compileProvider.preAssignBindingsEnabled(true);
+}]);
+
 // Configure AngularJS animation: do not animate fa-spin.
 igniteConsoleCfg.config(['$animateProvider', ($animateProvider) => {
     $animateProvider.classNameFilter(/^((?!(fa-spin)).)*$/);
