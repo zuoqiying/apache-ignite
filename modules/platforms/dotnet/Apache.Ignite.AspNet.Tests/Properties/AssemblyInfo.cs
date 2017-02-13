d72046dd61bba3acff25d961aaf2b703f01760b3
Ignite Teamcity <ignite@apache.org>
2017-02-13 14:36:46 +0300

    version changed to: 1.7.7

--- modules/platforms/dotnet/Apache.Ignite.AspNet.Tests/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.AspNet.Tests/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("18ea4c71-a11d-4ab1-8042-418f7559d84f")]
 
-[assembly: AssemblyVersion("1.8.0.16695")]
-[assembly: AssemblyFileVersion("1.8.0.16695")]
-[assembly: AssemblyInformationalVersion("1.8.0")]
+[assembly: AssemblyVersion("1.7.7.17847")]
+[assembly: AssemblyFileVersion("1.7.7.17847")]
+[assembly: AssemblyInformationalVersion("1.7.7")]
 
 [assembly: CLSCompliant(true)]
