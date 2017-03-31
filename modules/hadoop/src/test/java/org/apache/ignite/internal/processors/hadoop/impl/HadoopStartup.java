af9cdc7ae06255da29b58ff64ce39fb05b12f96b
iveselovskiy <iveselovskiy@gridgain.com>
2017-03-31 17:33:52 +0300

    4862: reverted unnecessary change.

--- modules/hadoop/src/test/java/org/apache/ignite/internal/processors/hadoop/impl/HadoopStartup.java
+++ modules/hadoop/src/test/java/org/apache/ignite/internal/processors/hadoop/impl/HadoopStartup.java
@@ -29,8 +29,7 @@ public class HadoopStartup {
      * @param args Arguments.
      */
     public static void main(String[] args) {
-        //G.start("config/hadoop/default-config.xml");
-        G.start("config/hadoop/config-4862.xml");
+        G.start("config/hadoop/default-config.xml");
     }
 
     /**
