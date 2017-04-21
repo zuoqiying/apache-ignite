35b2a4e30d34c1cec59dcddf43acb711e4108580
Alexander Belyak <alexandr.belyak@xored.com>
2017-04-21 15:07:00 +0300

    ignite-4927: Wake up non coalescing flushers after batch collected and fix test

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/integration/IgniteCacheTxStoreSessionWriteBehindCoalescingTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/integration/IgniteCacheTxStoreSessionWriteBehindCoalescingTest.java
@@ -17,11 +17,24 @@
 
 package org.apache.ignite.internal.processors.cache.integration;
 
+import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.CacheAtomicityMode;
 import static org.apache.ignite.cache.CacheAtomicityMode.TRANSACTIONAL;
+import org.apache.ignite.cache.store.CacheStore;
+import org.apache.ignite.cache.store.CacheStoreSession;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.lang.IgniteBiInClosure;
+import org.apache.ignite.resources.CacheStoreSessionResource;
+import org.apache.ignite.resources.IgniteInstanceResource;
+import org.jetbrains.annotations.Nullable;
+
+import javax.cache.Cache;
+import javax.cache.integration.CacheLoaderException;
+import javax.cache.integration.CacheWriterException;
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * Integration test write behind cache store with {@link CacheConfiguration#getWriteBehindCoalescing()}={@code False}
@@ -38,10 +51,46 @@ public class IgniteCacheTxStoreSessionWriteBehindCoalescingTest extends IgniteCa
     @Override protected IgniteConfiguration getConfiguration(String gridName) throws Exception {
         IgniteConfiguration result = super.getConfiguration(gridName);
 
-        CacheConfiguration ccfg = result.getCacheConfiguration()[0];
-
-        ccfg.setWriteBehindCoalescing(false);
+        for (CacheConfiguration ccfg : result.getCacheConfiguration()) {
+            ccfg.setWriteBehindCoalescing(false);
+            ccfg.setCacheStoreFactory(singletonFactory(new TestNonCoalescingStore()));
+        }
 
         return result;
+    }
+
+    /**
+     *
+     */
+    private class TestNonCoalescingStore extends TestStore {
+
+        /** {@inheritDoc} */
+        @Override public void writeAll(Collection<Cache.Entry<?, ?>> entries) throws CacheWriterException {
+            log.info("writeAll: " + entries);
+
+            assertTrue("Unexpected entries: " + entries, entries.size() <= 10);
+
+            checkSession("writeAll");
+
+            for (int i = 0; i < entries.size(); i++)
+                entLatch.countDown();
+        }
+
+        /** {@inheritDoc} */
+        @Override public void delete(Object key) throws CacheWriterException {
+            fail();
+        }
+
+        /** {@inheritDoc} */
+        @Override public void deleteAll(Collection<?> keys) throws CacheWriterException {
+            log.info("deleteAll: " + keys);
+
+            assertTrue("Unexpected keys: " + keys, keys.size() <= 10);
+
+            checkSession("deleteAll");
+
+            for (int i = 0; i < keys.size(); i++)
+                entLatch.countDown();
+        }
     }
 }
