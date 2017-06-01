4c184167ce92475fd8feb61e1be0d70846eead7c
Ignite Teamcity <ignite@apache.org>
2017-06-01 10:38:45 +0300

    version changed to: 1.7.11-p1

--- modules/platforms/dotnet/Apache.Ignite.AspNet.Tests/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.AspNet.Tests/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("18ea4c71-a11d-4ab1-8042-418f7559d84f")]
 
-[assembly: AssemblyVersion("1.8.0.16695")]
-[assembly: AssemblyFileVersion("1.8.0.16695")]
-[assembly: AssemblyInformationalVersion("1.8.0")]
+[assembly: AssemblyVersion("1.7.11.20435")]
+[assembly: AssemblyFileVersion("1.7.11.20435")]
+[assembly: AssemblyInformationalVersion("1.7.11-p1")]
 
 [assembly: CLSCompliant(true)]
--- modules/platforms/dotnet/Apache.Ignite.AspNet/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.AspNet/Properties/AssemblyInfo.cs
@@ -33,9 +33,9 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("13ea96fc-cc83-4164-a7c0-4f30ed797460")]
 
-[assembly: AssemblyVersion("1.8.0.16695")]
-[assembly: AssemblyFileVersion("1.8.0.16695")]
-[assembly: AssemblyInformationalVersion("1.8.0")]
+[assembly: AssemblyVersion("1.7.11.20435")]
+[assembly: AssemblyFileVersion("1.7.11.20435")]
+[assembly: AssemblyInformationalVersion("1.7.11-p1")]
 
 [assembly: CLSCompliant(true)]
 
