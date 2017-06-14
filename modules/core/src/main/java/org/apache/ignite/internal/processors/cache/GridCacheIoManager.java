2a894c3f3496afe62c1a7bc99498fb913824a28e
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2017-06-14 07:47:57 +0300

    Additional logging for marshaller sys cache

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheIoManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheIoManager.java
@@ -128,11 +128,17 @@ public class GridCacheIoManager extends GridCacheSharedManagerAdapter {
     /** Message listener. */
     private GridMessageListener lsnr = new GridMessageListener() {
         @Override public void onMessage(final UUID nodeId, final Object msg) {
-            if (log.isDebugEnabled())
+            final GridCacheMessage cacheMsg = (GridCacheMessage)msg;
+
+            if (log.isDebugEnabled()) {
                 log.debug("Received unordered cache communication message [nodeId=" + nodeId +
                     ", locId=" + cctx.localNodeId() + ", msg=" + msg + ']');
-
-            final GridCacheMessage cacheMsg = (GridCacheMessage)msg;
+            }
+            else if (CU.cacheId(CU.MARSH_CACHE_NAME) == cacheMsg.cacheId()) {
+                if (log.isInfoEnabled())
+                    log.info("Received cache communication message [nodeId=" + nodeId +
+                        ", locId=" + cctx.localNodeId() + ", msg=" + msg + ']');
+            }
 
             IgniteInternalFuture<?> fut = null;
 
@@ -205,6 +211,11 @@ public class GridCacheIoManager extends GridCacheSharedManagerAdapter {
 
                         log.debug(msg0.toString());
                     }
+                    else if (CU.cacheId(CU.MARSH_CACHE_NAME) == cacheMsg.cacheId()) {
+                        if (log.isInfoEnabled())
+                            log.info("Received message has higher affinity topology version [" +
+                                ", locId=" + cctx.localNodeId() + ", locAffVer=" + locAffVer + ", msg=" + msg + ']');
+                    }
 
                     fut = cctx.exchange().affinityReadyFuture(rmtAffVer);
                 }
@@ -225,6 +236,12 @@ public class GridCacheIoManager extends GridCacheSharedManagerAdapter {
 
                                     log.debug(msg0.toString());
                                 }
+                                else if (CU.cacheId(CU.MARSH_CACHE_NAME) == cacheMsg.cacheId()) {
+                                    if (log.isInfoEnabled())
+                                        log.info("Processing cache message after waiting for affinity topology " +
+                                            "version [rmtNodeId=" + nodeId + ", msg=" + cacheMsg + ']');
+                                }
+
 
                                 handleMessage(nodeId, cacheMsg);
                             }
@@ -900,6 +917,10 @@ public class GridCacheIoManager extends GridCacheSharedManagerAdapter {
 
         if (log.isDebugEnabled())
             log.debug("Sending cache message [msg=" + msg + ", node=" + U.toShortString(node) + ']');
+        else if (CU.cacheId(CU.MARSH_CACHE_NAME) == msg.cacheId()) {
+            if (log.isInfoEnabled())
+                log.info("Sending cache message [msg=" + msg + ", node=" + U.toShortString(node) + ']');
+        }
 
         int cnt = 0;
 
