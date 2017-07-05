fd6b2401cc107c0911c749c8e433ce6566bf08cc
oleg-ostanin <oostanin@gridgain.com>
2017-07-05 19:34:20 +0300

    edited IgniteBenchmarkArguments

--- modules/yardstick/src/main/java/org/apache/ignite/yardstick/IgniteBenchmarkArguments.java
+++ modules/yardstick/src/main/java/org/apache/ignite/yardstick/IgniteBenchmarkArguments.java
@@ -225,18 +225,6 @@ public class IgniteBenchmarkArguments {
     private boolean printRollBacks;
 
     /** */
-    @Parameter(names = {"-sl", "--stringLength"}, description = "Test string length")
-    private int stringLength = 12;
-
-    /** */
-    @Parameter(names = {"-wt", "--warningTime"}, description = "Warning time interval for printing log")
-    private long warningTime = 500;
-
-    /** */
-    @Parameter(names = {"-prb", "--printRollBacks"}, description = "Print rollBacks")
-    private boolean printRollBacks;
-
-    /** */
     @Parameter(names = {"-cg", "--cacheGrp"}, description = "Cache group for caches")
     private String cacheGrp;
 
@@ -528,27 +516,6 @@ public class IgniteBenchmarkArguments {
      */
     public int getPageSize() {
         return pageSize;
-    }
-
-    /**
-     * @return Test string length.
-     */
-    public int getStringLength() {
-        return stringLength;
-    }
-
-    /**
-     * @return Warning time interval.
-     */
-    public long getWarningTime() {
-        return warningTime;
-    }
-
-    /**
-     * @return Flag for printing rollbacks.
-     */
-    public boolean printRollBacks() {
-        return printRollBacks;
     }
 
     /**
