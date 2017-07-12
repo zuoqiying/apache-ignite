aade34220959d5fe56245f70594164df84baca1e
Igor Sapego <isapego@gridgain.com>
2017-07-12 16:04:10 +0300

    IGNITE-5708: Added tests

--- modules/platforms/cpp/core-test/src/binary_any_test.cpp
+++ modules/platforms/cpp/core-test/src/binary_any_test.cpp
@@ -23,11 +23,38 @@
 
 #include <ignite/binary/binary_any.h>
 
+using namespace ignite;
 using namespace ignite::binary;
+using namespace ignite::common;
 
 bool IsAnyCastError(const ignite::IgniteError& err)
 {
     return err.GetCode() == ignite::IgniteError::IGNITE_ERR_ANY_CAST;
+}
+
+template<typename T>
+void TestBasicValid(const T& actual)
+{
+    BinaryAny any(actual);
+
+    BOOST_CHECK(!any.IsEmpty());
+
+    T val = BinaryAnyCast<T>(any);
+    BOOST_CHECK(val == actual);
+
+    T& ref = BinaryAnyCast<T&>(any);
+    BOOST_CHECK(ref == actual);
+
+    const T& cref = BinaryAnyCast<const T&>(any);
+    BOOST_CHECK(cref == actual);
+
+    T* ptr = BinaryAnyCast<T*>(any);
+    BOOST_CHECK(ptr != 0);
+    BOOST_CHECK(*ptr == actual);
+
+    const T* cptr = BinaryAnyCast<const T*>(any);
+    BOOST_CHECK(cptr != 0);
+    BOOST_CHECK(*cptr == actual);
 }
 
 BOOST_AUTO_TEST_SUITE(BinaryAnyTestSuite)
@@ -47,6 +74,86 @@ BOOST_AUTO_TEST_CASE(Empty)
 
     const std::string* cptr = BinaryAnyCast<const std::string*>(any);
     BOOST_CHECK(cptr == 0);
+}
+
+BOOST_AUTO_TEST_CASE(BasicInt8)
+{
+    TestBasicValid<int8_t>(42);
+    TestBasicValid<int8_t>(0);
+    TestBasicValid<int8_t>(-127);
+}
+
+BOOST_AUTO_TEST_CASE(BasicInt16)
+{
+    TestBasicValid<int16_t>(4242);
+    TestBasicValid<int16_t>(0);
+    TestBasicValid<int16_t>(-32768);
+}
+
+BOOST_AUTO_TEST_CASE(BasicInt32)
+{
+    TestBasicValid<int32_t>(42424242);
+    TestBasicValid<int32_t>(0);
+    TestBasicValid<int32_t>(-2147483647);
+}
+
+BOOST_AUTO_TEST_CASE(BasicInt64)
+{
+    TestBasicValid<int64_t>(4242424242424242);
+    TestBasicValid<int64_t>(0);
+    TestBasicValid<int64_t>(-9223372036854775807);
+}
+
+BOOST_AUTO_TEST_CASE(BasicBool)
+{
+    TestBasicValid<bool>(true);
+    TestBasicValid<bool>(false);
+}
+
+BOOST_AUTO_TEST_CASE(BasicFloat)
+{
+    TestBasicValid<float>(42.42f);
+    TestBasicValid<float>(0.0f);
+    TestBasicValid<float>(-1.0f);
+}
+
+BOOST_AUTO_TEST_CASE(BasicDouble)
+{
+    TestBasicValid<double>(42.42);
+    TestBasicValid<double>(0.0);
+    TestBasicValid<double>(-1.0);
+}
+
+BOOST_AUTO_TEST_CASE(BasicGuid)
+{
+    TestBasicValid<Guid>(Guid(0x198A423B4FE96730LL, 0xD82AB007263F82C6LL));
+    TestBasicValid<Guid>(Guid(0, 0));
+}
+
+BOOST_AUTO_TEST_CASE(BasicDate)
+{
+    TestBasicValid<Date>(MakeDateGmt(1989, 10, 26));
+    TestBasicValid<Date>(MakeDateGmt(2057, 01, 31));
+}
+
+BOOST_AUTO_TEST_CASE(BasicTimestamp)
+{
+    TestBasicValid<Timestamp>(MakeTimestampGmt(1998, 12, 27, 1, 2, 3, 456));
+    TestBasicValid<Timestamp>(MakeTimestampGmt(2057, 01, 31, 14, 29, 10, 110394867));
+}
+
+BOOST_AUTO_TEST_CASE(BasicTime)
+{
+    TestBasicValid<Time>(MakeTimeGmt(1, 2, 3));
+    TestBasicValid<Time>(MakeTimeGmt(0, 0, 0));
+    TestBasicValid<Time>(MakeTimeGmt(14, 29, 10));
+}
+
+BOOST_AUTO_TEST_CASE(BasicString)
+{
+    TestBasicValid<std::string>("Lorem ipsum");
+    TestBasicValid<std::string>("");
+    TestBasicValid<std::string>("x");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
