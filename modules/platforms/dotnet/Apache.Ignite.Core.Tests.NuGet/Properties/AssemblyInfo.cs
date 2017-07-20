9e9a88f95759095b47a42779659737022e822ad8
Ignite Teamcity <ignite@apache.org>
2017-07-20 16:50:26 +0000

    version changed to: 1.7.12-p1

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("134707f6-155d-47f6-9eb2-c67abbf3c009")]
 
-[assembly: AssemblyVersion("1.8.0.16695")]
-[assembly: AssemblyFileVersion("1.8.0.16695")]
-[assembly: AssemblyInformationalVersion("1.8.0")]
+[assembly: AssemblyVersion("1.7.12.21617")]
+[assembly: AssemblyFileVersion("1.7.12.21617")]
+[assembly: AssemblyInformationalVersion("1.7.12-p1")]
